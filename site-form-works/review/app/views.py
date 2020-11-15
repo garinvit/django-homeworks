dbd78680f8c9ffe93b6fd33ad5da28468dc9ed7d
garinvit <garin.vitaly@yandex.ru>
2020-11-15 23:43:04 +0700

    review hw add

--- site-form-works/review/app/views.py
+++ site-form-works/review/app/views.py
@@ -19,15 +19,22 @@ def product_list_view(request):
 def product_view(request, pk):
     template = 'app/product_detail.html'
     product = get_object_or_404(Product, id=pk)
-
+    text = Review.objects.select_related('product')
     form = ReviewForm
-    if request.method == 'POST':
+    if request.method == 'POST' and ('is_review_exist' not in request.session):
         # логика для добавления отзыва
-        pass
-
+        review_form = ReviewForm(request.POST)
+        if review_form.is_valid():
+            comment = review_form.save(commit=False)
+            comment.product = Product.objects.get(id=pk)
+            review_form.save()
+            request.session.set_expiry(60)
+            request.session['is_review_exist'] = True
+    is_review_exist = request.session.get('is_review_exist')
     context = {
         'form': form,
-        'product': product
+        'product': product,
+        'reviews': text,
+        'is_review_exist': is_review_exist
     }
-
     return render(request, template, context)
